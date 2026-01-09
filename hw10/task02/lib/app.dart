4c4fff6d7e95bdfe04eae205a915aca3532302f6
barabuly83 <barabuly83@gmail.com>
2026-01-09 15:53:06 +0300

    Рефакторинг task02: разделение виджетов на файлы и исправление анимации
    - Разделены виджеты на отдельные файлы согласно best practices
    - Создана структура: main.dart, app.dart, views/position_animation_screen.dart
    - Удалены избыточные комментарии
    - Исправлена анимация AnimatedPositioned (использование конкретных значений вместо null)
    - Обновлен тест для корректного импорта

---
+++ hw10/task02/lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'views/position_animation_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Position Animation',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const PositionAnimationScreen(),
+    );
+  }
+}
+
